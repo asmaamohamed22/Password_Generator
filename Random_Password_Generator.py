9407229812ceb3a68df491e606c56e533ba749da
mohraeel <61949896+mohraeel@users.noreply.github.com>
2021-01-04 23:12:48 +0200

    Update Random_Password_Generator.py

--- Random_Password_Generator.py
+++ Random_Password_Generator.py
@@ -13,7 +13,6 @@ def low():
     digits = "ABCDEFGHIJKLMNOPRSTUVWXYZabcdefghijklmnoprstuvwxyz1234567890 !@#$%^&*()~"
     password = ""
     
-    
     if var.get() == 1:
         for i in range(0,lenght):
             password = password + random.choice(lower)
@@ -30,12 +29,12 @@ def low():
         return password
     else:
         print("Please Choose an option")
-        
+   
+
 def generate():
     password1 = low()
     entry.insert(10,password1)
-
-
+    
 def copyclip():
     random_password = entry.get()
     pyperclip.copy(random_password)
@@ -53,9 +52,6 @@ Random_password.place(x=20,y=30)
 entry = Entry(root)
 entry.place(x=90,y=30,width= 200)
 
-
-
-
 c_label = Label(root,text="Length")
 c_label.place(x=20,y=70)
 
@@ -65,6 +61,13 @@ copy_button.place(x=40,y=180,width=110)
 generate_button = Button(root,text="Generate",command=generate)
 generate_button.place(x=160,y=180,width=110)
 
+radio_low = Radiobutton(root,text="Low",variable=var,value=1)
+radio_low.place(x=50,y=120)
 
+radio_med = Radiobutton(root,text="Medium",variable=var,value=0)
+radio_med.place(x=120, y=120)
+
+radio_strong = Radiobutton(root,text="Strong",variable=var,value=3)
+radio_strong.place(x=200, y=120)
 
 
